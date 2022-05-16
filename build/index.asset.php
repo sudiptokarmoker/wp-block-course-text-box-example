a33d39fc2e21a9cc4fe5490906e03e55782bb708
sudipto <sudiptocsi@gmail.com>
2022-05-16 15:28:21 +0600

    added panel color settings & contrast checker

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '49dcd4194b7acfd1c8b2f899169487d2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5a9205d271d50ccda77a6e281de002f8');
--- src/edit.js
+++ src/edit.js
@@ -5,21 +5,14 @@ import {
 	BlockControls,
 	InspectorControls,
 	AlignmentToolbar,
+	PanelColorSettings,
+	ContrastChecker,
 } from '@wordpress/block-editor';
-import {
-	PanelBody,
-	TextControl,
-	TextareaControl,
-	ToggleControl,
-	AnglePickerControl,
-	ColorPicker,
-	ColorPalette,
-} from '@wordpress/components';
 
 import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
-	const { text, alignment } = attributes;
+	const { text, alignment, backgroundColor, textColor } = attributes;
 	const onChangeText = (newText) => {
 		setAttributes({
 			text: newText,
@@ -30,46 +23,39 @@ export default function Edit({ attributes, setAttributes }) {
 			alignment: newAlignment,
 		});
 	};
+	const onBackgroundColorChange = (newBgColor) => {
+		setAttributes({ backgroundColor: newBgColor });
+	};
+	const onTextColorChange = (newTextColor) => {
+		setAttributes({ textColor: newTextColor });
+	};
 	return (
 		<>
 			{/* here on previous commit (if you want just then see the last commit of this code) we just putted here some lot of codeon placing toolbar by @wordpress/components package. But now we have just here working by another way!*/}
 			{/* https://github.com/sudiptokarmoker/wp-block-course-text-box-example/commit/8fb9045275177c2d50448339be170eb21c2931a0 */}
 			<InspectorControls>
-				<PanelBody
-					title={__('Color Settings', 'text-box')}
+				<PanelColorSettings
+					title={__('Color settings', 'text-box')}
 					icon="admin-appearance"
 					initialOpen
+					colorSettings={[
+						{
+							value: backgroundColor,
+							onChange: onBackgroundColorChange,
+							label: __('Background Color', 'text-box'),
+						},
+						{
+							value: textColor,
+							onChange: onTextColorChange,
+							label: __('Text Color', 'text-box'),
+						},
+					]}
 				>
-					<TextControl
-						label="Inpu Label"
-						value={text}
-						onChange={onChangeText}
-						help="help text"
+					<ContrastChecker
+						textColor={textColor}
+						backgroundColor={backgroundColor}
 					/>
-					<TextareaControl
-						label="Text Area Label"
-						value={text}
-						onChange={onChangeText}
-						help="help text"
-					/>
-					<ToggleControl
-						label="Toggle Label"
-						checked={true}
-						//onChange={(v) => console.log(v)}
-					/>
-					<AnglePickerControl />
-					<ColorPicker
-						color={' F03 '}
-						//onChangeComplete={(v) => console.log(v)}
-					/>
-					<ColorPalette
-						colors={[
-							{ name: 'red', color: '#F00' },
-							{ name: 'black', color: '#000' },
-						]}
-						//onChange={ (v) => console.log(v) }
-					/>
-				</PanelBody>
+				</PanelColorSettings>
 			</InspectorControls>
 			<BlockControls>
 				<AlignmentToolbar
@@ -80,6 +66,10 @@ export default function Edit({ attributes, setAttributes }) {
 			<RichText
 				{...useBlockProps({
 					className: `text-box-align-${alignment}`,
+					style: {
+						backgroundColor,
+						color: textColor,
+					},
 				})}
 				onChange={onChangeText}
 				value={text}
